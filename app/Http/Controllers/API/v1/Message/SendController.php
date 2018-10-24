5013bc282b18b797d4161862a88aaf289569c13b
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2018-10-24 13:48:26 +0800

    Bug fix
    Disallow sending messages to self

--- app/Http/Controllers/API/v1/Message/SendController.php
+++ app/Http/Controllers/API/v1/Message/SendController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\v1\Message;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Util;
 use App\Model\Message;
 
 class SendController extends Controller
@@ -15,12 +16,15 @@ class SendController extends Controller
             'messages' => 'required'
         ]);
 
+        if ($request->user()->id == $id)
+            return Util::err('You can\'t message yourself');
+
         $messages = new Message;
         $messages->messages = $request->messages;
         $messages->from = $request->user()->id;
         $messages->to = $id;
         $messages->save();
 
-        return (new GetController)->user($request, $id);        
+        return (new GetController)->user($request, $id);
     }
 }
